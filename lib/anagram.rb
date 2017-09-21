3506e5c7de7a1171d9bc76b6199c749fd6b84374
lainey <laineyNrokk@gmail.com>
2017-09-21 10:51:03 -0700

    use the is_word? methode to check the input string is a real word

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,14 @@
 class Anagram
   def initialize (word)
     @word = word
+  end
 
-    attr_acessor :word
+  attr_accessor :word
+
+  def is_word?(word)
+    if (word=~ /[aeiou]/) == nil
+      return false
+    else
+      return true
+    end
+  end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -3,3 +3,9 @@ require ('anagram')
 
 testObj0 = Anagram.new("David")
 testOj = Anagram.new("trcsln")
+
+describe('Anagram#is_word?') do
+  it('checks if input strings are real words by determining if they contain vowels or not') do
+    expect(testObj.is_word?("trcsln")).to(eq(false))
+  end
+end
